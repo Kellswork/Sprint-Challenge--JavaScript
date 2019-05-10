7925690bf63c2a05e7e8e7461658c996bc489045
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-05-10 08:58:28 +0100

    add solutions to advanced array-methods questions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -244,7 +244,8 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+let lowerCase = [];
+lowerCase = zooAnimals.map(zooAnimal => zooAnimal.animal_name.toLowerCase());
 console.log(lowerCase);
 
 /* Request 3: .filter() 
@@ -252,7 +253,8 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+let largerPopulation = [];
+largerPopulation = zooAnimals.filter(zooAnimal => zooAnimal.population < 5);
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -260,7 +262,11 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+let populationTotal = 0;
+populationTotal = zooAnimals.reduce(
+  (total, currentValue) => total + currentValue.population,
+  0
+);
 console.log(populationTotal);
 
 /* 
